0f7e9274ba708e836edf5f8baa959e398270547b
Bhavyavyas10 <bbvyas1082000@gmail.com>
2021-06-17 00:42:15 +0530

    SafeMath contract contains declaration and definition of all arithmatic functions.

--- ERC.sol
+++ ERC.sol
@@ -1,14 +1,19 @@
 pragma solidity ^0.5.0;
 
-contract ERC20Interface {
-    function totalSupply() public view returns (uint);
-    function balanceOf(address tokenOwner) public view returns (uint balance);
-    function allowance(address tokenOwner, address spender) public view returns (uint remaining);
-    function transfer(address to, uint tokens) public returns (bool success);
-    function approve(address spender, uint tokens) public returns (bool success);
-    function transferFrom(address from, address to, uint tokens) public returns (bool success);
 
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+contract SafeMath {
+    function safeAdd(uint a, uint b) public pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+    function safeSub(uint a, uint b) public pure returns (uint c) {
+        require(b <= a); c = a - b; } 
+    function safeMul(uint a, uint b) public pure returns (uint c) { 
+        c = a * b; require(a == 0 || c / a == b); } 
+    function safeDiv(uint a, uint b) public pure returns (uint c) { 
+        require(b > 0);
+        c = a / b;
+    }
 }
+
 
