8502f05b015ed9af19a538bacd6e421fa8463c91
Bhavyavyas10 <bbvyas1082000@gmail.com>
2021-06-17 00:44:48 +0530

    TestToken contract contains declaration and definition of all functions which plays major role for Token.

--- ERC.sol
+++ ERC.sol
@@ -1,19 +1,56 @@
 pragma solidity ^0.5.0;
 
 
-contract SafeMath {
-    function safeAdd(uint a, uint b) public pure returns (uint c) {
-        c = a + b;
-        require(c >= a);
+contract TestToken is ERC20Interface, SafeMath {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint public time;
+
+    uint256 public _totalSupply;
+
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+
+    constructor() public {
+        name = "TestToken";
+        symbol = "TT";
+        decimals = 18;
+        _totalSupply = 1000000;
+        time = block.timestamp;
+        balances[msg.sender] = _totalSupply;
+        emit Transfer(address(0), msg.sender, _totalSupply);
     }
-    function safeSub(uint a, uint b) public pure returns (uint c) {
-        require(b <= a); c = a - b; } 
-    function safeMul(uint a, uint b) public pure returns (uint c) { 
-        c = a * b; require(a == 0 || c / a == b); } 
-    function safeDiv(uint a, uint b) public pure returns (uint c) { 
-        require(b > 0);
-        c = a / b;
+    
+    modifier lock(){
+        require(block.timestamp >= time);
+        _;
     }
+
+    function totalSupply() public view returns (uint) {
+        return _totalSupply  - balances[address(0)];
+    }
+
+    function balanceOf(address tokenOwner) public view returns (uint balance) {
+        return balances[tokenOwner];
+    }
+
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+
+    function approve(address spender, uint tokens) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+    function transfer(address to, uint tokens) public lock returns (bool success) {
+        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        time = time+600;
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+
 }
-
-
